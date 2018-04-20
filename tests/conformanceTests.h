5fff38dde0fa14d44fc59be4036131ea1a8625c1
Dominik Harmim <harmim6@gmail.com>
2018-04-20 18:08:22 +0100

    Tests

---
+++ tests/conformanceTests.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void runConformanceTests(char const *groundTruth);
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ tests/performanceTest.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void runPerformanceTest();
+
+
+#ifdef __cplusplus
+}
+#endif
