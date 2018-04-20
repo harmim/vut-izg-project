90bbfca6aef3e6c95d2b804e85b421f2a2f0c17a
Dominik Harmim <harmim6@gmail.com>
2018-04-20 02:06:50 +0100

    CMake

--- merlinCompilationTest.sh
+++ merlinCompilationTest.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-mv CMakeLists.txt CMakeLists_mac.txt
-mv CMakeLists_original.txt CMakeLists.txt
-
 mkdir -p build
-(cd build && cmake -D CMAKE_BUILD_TYPE=DEBUG -D SDL2_DIR=/mnt/matylda1/imilet/installedLibs/lib/cmake/SDL2 .. && make)
-
-mv CMakeLists.txt CMakeLists_original.txt
-mv CMakeLists_mac.txt CMakeLists.txt
+cd build
+cmake -D CMAKE_BUILD_TYPE=DEBUG -D SDL2_DIR=/mnt/matylda1/imilet/installedLibs/lib/cmake/SDL2 ..
+make
