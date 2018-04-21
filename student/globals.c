2e5201da030b7d180e30bd72c127ec4767413837
Dominik Harmim <harmim6@gmail.com>
2018-04-21 16:34:51 +0100

    Globals

---
+++ student/globals.c
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief This file contains global variables
+ *
+ * @author Tomáš Milet, imilet@fit.vutbr.cz
+ * @author Dominik Harmim <harmim6@gmail.com>
+ */
+
+
+#include <student/globals.h>
+
+
+/// This variable contains view matrix of orbit camera.
+Mat4 viewMatrix;
+/// This variable contains projection matrix of orbit camera.
+Mat4 projectionMatrix;
+/// This variable contains camera position in world-space.
+Vec3 cameraPosition;
---
+++ student/globals.h
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * @brief This file contains global variables.
+ *
+ * @author Tomáš Milet, imilet@fit.vutbr.cz
+ * @author Dominik Harmim <harmim6@gmail.com>
+ */
+
+
+#pragma once
+
+
+#include <student/linearAlgebra.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/// This variable contains view matrix of orbit camera.
+extern Mat4 viewMatrix;
+/// This variable contains projection matrix of orbit camera.
+extern Mat4 projectionMatrix;
+/// This variable contains camera position in world-space.
+extern Vec3 cameraPosition;
+
+
+#ifdef __cplusplus
+}
+#endif
